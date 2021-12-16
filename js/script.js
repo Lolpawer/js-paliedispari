1ca29ea7723169550f01a3545d9669c66b9a7369
Lolpawer <dadu.petrosino@gmail.com>
2021-12-16 17:55:24 +0100

    even and odd

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 const palindromeOutput = document.getElementById("palindrome-output");
 const palindromeClick = document.getElementById("palindrome-button");
 
-function palindrome(input) {
+function palindrome(paliInput) {
 
-    const wordLenght = input.length;
+    const wordLenght = paliInput.length;
 
     for (let i = 0; i < wordLenght / 2; i++) {
-        if (input[i] !== input[wordLenght - 1 - i]) {
+        if (paliInput[i] !== paliInput[wordLenght - 1 - i]) {
             return 'La parola non è un palindromo';
         }
     }
@@ -21,3 +21,59 @@ palindromeClick.addEventListener ('click', function() {
     palindromeOutput.innerHTML = palindromeResult;
 });
 
+//----------------------------------------------------
+
+const evenClick = document.getElementById('even-button');
+const oddClick = document.getElementById('odd-button');
+const playerEvenOdd = document.getElementById('evenodd-player-output');
+const cpuEvenOdd = document.getElementById('evenodd-cpu-output');
+const resultEvenOdd = document.getElementById('evenodd-result-output');
+let playerInput;
+
+function cpuRandom (min, max) {
+    let cpuResult = Math.floor(Math.random() * (max - min + 1) + min);
+    return cpuResult;
+}
+
+function evenOdd (sum) {
+    if (sum % 2 === 0) {
+        let winner = "even";
+        return winner;
+    }
+}
+
+evenClick.addEventListener ('click', function() {
+    playerInput = prompt("Inserisci un numero da 1 a 5");
+    playerEvenOdd.innerHTML = `Giocatore: ${playerInput}`;
+    if (playerInput < 1 || playerInput > 5) {
+        resultEvenOdd.innerHTML = "Il numero che hai scelto non è tra 1 e 5";
+    } else {
+        let cpuInput = cpuRandom (1, 5);
+        cpuEvenOdd.innerHTML = `CPU: ${cpuInput}`;
+        let sum = parseInt(playerInput) + parseInt(cpuInput);
+        let result = evenOdd(sum);
+        if (result === "even") {
+            resultEvenOdd.innerHTML = `La somma è ${sum}, vince il giocatore.`;
+        } else {
+            resultEvenOdd.innerHTML = `La somma è ${sum}, vince il computer.`;
+        }
+    }
+});
+
+oddClick.addEventListener ('click', function() {
+    playerInput = prompt("Inserisci un numero da 1 a 5");
+    playerEvenOdd.innerHTML = `Giocatore: ${playerInput}`;
+    if (playerInput < 1 || playerInput > 5) {
+        resultEvenOdd.innerHTML = "Il numero che hai scelto non è tra 1 e 5";
+    } else {
+        let cpuInput = cpuRandom (1, 5);
+        cpuEvenOdd.innerHTML = `CPU: ${cpuInput}`;
+        let sum = parseInt(playerInput) + parseInt(cpuInput);
+        let result = evenOdd(sum);
+        if (result != "even") {
+            resultEvenOdd.innerHTML = `La somma è ${sum}, vince il giocatore.`;
+        } else {
+            resultEvenOdd.innerHTML = `La somma è ${sum}, vince il computer.`;
+        }
+    }
+});
